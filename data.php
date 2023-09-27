7c8a0da7122753fafd9794e0a6614bacb628b245
Chalvinhuby55 <cjemihuby@gmail.com>
2023-09-27 15:35:28 +0900

    Latihan Function

---
+++ data.php
@@ -0,0 +1,26 @@
+<?php
+
+    // Date
+    // Untuk menampilkan tanggal dengan format tertentu
+    //  echo date ('l, d-M-Y');
+
+    // Time
+    // UNIX Timestamp / EPOCH time
+    // Detik yang sudah berlalu sejak 1 Januari 1970
+    //  echo time ();
+    // echo date ('l', time()  -60*60*24*100 );
+
+
+    // mktime
+    // Membuat sendiri detik
+    // mktime (0,0,0,0,0,0)
+    // jam, menit, detik, bulan, tanggal, tahun
+    // echo date ("l", mktime(0,0,0,4,06,1999));
+
+    // strtotime
+    // echo strtotime("04 july 1999");
+
+  
+
+?>
+
